d9535bd421ca961547183ede959d22245effe204
nucl34rbyte <nucl34rbyte@protonmail.com>
2018-04-10 11:15:22 +0500

    JSClocky v1.0

---
+++ js/index.js
@@ -0,0 +1,27 @@
+var clock = document.getElementById('clock');
+var hexcolor = document.getElementById('hexcolor');
+
+function updateTime(){
+	var dTime = new Date();
+	var h = dTime.getHours().toString();
+	var m = dTime.getMinutes().toString();
+	var s = dTime.getSeconds().toString();
+
+
+	if(h.length < 2){
+		h = '0' + m;
+	}
+	if(m.length < 2){
+		m = '0' + m;
+	}
+	if(s.length < 2){
+		s = '0' + s;
+	}
+	clock.innerText = h + ':' + m + ':' + s;
+
+	hexcolor.innerText = '#' + h + m + s;
+	document.body.style.background = hexcolor.innerText;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
